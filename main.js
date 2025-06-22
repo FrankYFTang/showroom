6d349da6fa0a92df14ff68475e3bf8680934cefd
Frank Yung-Fong Tang <ftang@google.com>
2025-06-21 23:52:53 -0700

    remove flakeyness

--- main.js
+++ main.js
@@ -286,8 +286,8 @@ let wallInfo = [
   {width: wallDepth,        depth:20 + 2 * wallDepth, height: wallHeight, x: -wallDepth, y:0, z: -wallDepth+eps}, // e
   {width: wallDepth,        depth:20 + 2 * wallDepth, height: wallHeight, x: 20, y:0, z: -wallDepth+eps},  // n
   {width: wallDepth,        depth:4, x: 10-wallDepth/2.0, height: wallHeight, y:0, z: 20-4+eps}, // h and l
-  {width: wallDepth,        depth:8, x: 5.5, height: wallHeight, y:0, z: 5+eps}, // f and i
-  {width: wallDepth,        depth:8, x: 14, height: wallHeight, y:0, z: 5+eps}, // k and o
+  {width: wallDepth,        depth:8, x: 5.5-eps, height: wallHeight, y:0, z: 5+eps}, // f and i
+  {width: wallDepth,        depth:8, x: 14+eps, height: wallHeight, y:0, z: 5+eps}, // k and o
   {width: 8+2*wallDepth, depth:wallDepth, height: wallHeight, x: 5.5, y:0, z: 5}, // c and j
 ];
 const wallA = wallInfo[0];
