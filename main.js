7b96f3a8dff07ffa6e513314445ec8f5f993a5d6
Frank Yung-Fong Tang <ftang@google.com>
2025-06-21 23:01:26 -0700

    Fix

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const jumpNumOfWall = 5;
 const wallColor1 = 'ivory';
 const wallColor2 = 'whitesmoke';
 const skyColor = 'dimgray';
-const eps = 0.02;
+const eps = 0.01;
 
                         import * as THREE from 'three';
 			import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
@@ -286,7 +286,7 @@ let wallInfo = [
   {width: wallDepth,        depth:20 + 2 * wallDepth, height: wallHeight, x: -wallDepth, y:0, z: -wallDepth+eps}, // e
   {width: wallDepth,        depth:20 + 2 * wallDepth, height: wallHeight, x: 20, y:0, z: -wallDepth+eps},  // n
   {width: wallDepth,        depth:4, x: 10-wallDepth/2, height: wallHeight, y:0, z: 20-4+eps}, // h and l
-  {width: wallDepth,        depth:8, x: (20-(8+2*wallDepth))/2, height: wallHeight, y:0, z: 5+eps}, // f and i
+  {width: wallDepth,        depth:8, x: wallDepth+(20-(8+2*wallDepth))/2, height: wallHeight, y:0, z: 5+eps}, // f and i
   {width: wallDepth,        depth:8, x: (8+2*wallDepth)+(20-(8+2*wallDepth))/2, height: wallHeight, y:0, z: 5+eps}, // k and o
   {width: 8+2*wallDepth, depth:wallDepth, height: wallHeight, x: (20-(8+2*wallDepth))/2, y:0, z: 5}, // c and j
 ];
