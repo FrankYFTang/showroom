f7cf23862c480fc0fee2ff2edb8c5fca86ffc779
Frank Yung-Fong Tang <ftang@google.com>
2025-06-29 15:57:43 -0700

    fix

--- main.js
+++ main.js
@@ -672,7 +672,7 @@ function initWallL() {
 	for (let i = 0; i < paintings.length; i++) {
 		const painting =  paintings[i];
 		if ( painting ) {
-                        addFrameArtwork(painting.name, frameGeometry, undefined, x, (i % 2 == 0) ? upperY : lowerY, (wallH.z+1+ (i-(i%2)) * scale, -eps, 0, frameDepth, painting.height, painting.width);
+                        addFrameArtwork(painting.name, frameGeometry, undefined, x, (i % 2 == 0) ? upperY : lowerY, (wallH.z+1+ (i-(i%2))) * scale, -eps, 0, frameDepth, painting.height, painting.width);
 		}
 	}
 }
